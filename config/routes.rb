748bea79f22f298287fc94145ad762f5b4f8a39d
Micah Alleyne <micah.alleyne@hgs.hiddengeniusproject.org>
2019-12-04 17:57:43 -0800

    Created links edit css

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'athletes', :to => 'home#athletes'
+  get 'index', :to => 'home#index'
+  get 'news', :to => 'home#news'
 end
